183173c85e77a489779640ff558aa6833f9256c8
Tilen Majerle <tilen.majerle@gmail.com>
2022-10-23 11:22:12 +0200

    Very first commit

---
+++ dev/main.c
@@ -0,0 +1,12 @@
+/*
+ * main.c : Defines the entry point for the console application.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "lwbtn/lwbtn.h"
+
+int
+main(void) {
+    return 0;
+}
---
+++ lwbtn/src/include/lwbtn/lwbtn.h
@@ -0,0 +1,62 @@
+/**
+ * \file            lwbtn.h
+ * \brief           Lightweight button manager
+ */
+
+/*
+ * Copyright (c) 2022 Tilen MAJERLE
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * This file is part of LwBTN - Lightweight button manager.
+ *
+ * Author:          Tilen MAJERLE <tilen@majerle.eu>
+ * Version:         v0.0.0
+ */
+#ifndef LWBTN_HDR_H
+#define LWBTN_HDR_H
+
+#include <stdint.h>
+#include <string.h>
+#include "lwbtn/lwbtn_opt.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * \defgroup        LWBTN Lightweight button manager
+ * \brief           Lightweight button manager
+ * \{
+ */
+
+void lwbtn_init(void);
+void lwbtn_process(uint32_t mstime);
+
+/**
+ * \}
+ */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* LWBTN_HDR_H */
