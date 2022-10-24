d1cd4b89da6f61f3002b1ef62d8a55c88f840c41
Tilen Majerle <tilen@majerle.eu>
2022-10-24 09:02:18 +0200

    Simple simulator

--- dev/main.c
+++ dev/main.c
@@ -1,12 +1,24 @@
-/*
- * main.c : Defines the entry point for the console application.
- */
-
 #include <stdio.h>
 #include <string.h>
 #include "lwbtn/lwbtn.h"
+#include "windows.h"
+
+const int keys[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int state[sizeof(keys) / sizeof(keys[0])];
+int state_prev[sizeof(keys) / sizeof(keys[0])];
 
 int
 main(void) {
-    return 0;
+    printf("Application running\r\n");
+
+    while (1) {
+        /* Simple simulator */
+        for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); ++i) {
+            state[i] = GetAsyncKeyState(keys[i]) < 0;
+            if (state[i] != state_prev[i]) {
+                printf("State changed for key %c to: %d\r\n", keys[i], state[i]);
+            }
+            state_prev[i] = state[i];
+        }
+    }
 }
