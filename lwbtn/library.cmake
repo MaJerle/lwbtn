db3e26cd4366e177e044ce74284e520cc7542c82
Tilen Majerle <tilen.majerle@gmail.com>
2023-12-09 16:49:22 +0100

    Improve the cmake documentation

--- lwbtn/library.cmake
+++ lwbtn/library.cmake
@@ -1,3 +1,14 @@
+# 
+# This file provides set of variables for end user
+# and also generates one (or more) libraries, that can be added to the project using target_link_libraries(...)
+#
+# Before this file is included to the root CMakeLists file (using include() function), user can set some variables:
+#
+# LWBTN_OPTS_DIR: If defined, it should set the folder path where options file shall be generated.
+# LWBTN_COMPILE_OPTIONS: If defined, it provide compiler options for generated library.
+# LWBTN_COMPILE_DEFINITIONS: If defined, it provides "-D" definitions to the library build
+#
+
 # Library core sources
 set(lwbtn_core_SRCS 
     ${CMAKE_CURRENT_LIST_DIR}/src/lwbtn/lwbtn.c
